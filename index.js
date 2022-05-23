715d4464950f81566624efaca2abcd904ee7dc79
tomas lanterna <tomas.lanterna@hotmail.com>
2022-05-23 20:34:44 -0300

    cambio de tiempo timeout

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.Promise=global.Promise;
 mongoose.connect('mongodb+srv://tomi:Sanantonio20$@restoappcluster.iunel.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then(() => {
         console.log('La conexion a la db se a realizado bien');
-        //Crear servidor y me pongo a escuchar peticiones http
+        //Crear servidor y me pongo a escuchar peticiones https
 
         app.listen(port,()=>{
             console.log('servidor corriendo en http://localhost:'+port);
