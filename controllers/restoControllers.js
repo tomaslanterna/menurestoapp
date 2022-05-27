a5f0169a1aaced4629dceaeb0b66f3f724d136f5
tomas lanterna <tomas.lanterna@hotmail.com>
2022-05-27 15:53:11 -0300

    Se agrega token para ruta resto get

--- controllers/restoControllers.js
+++ controllers/restoControllers.js
@@ -1,121 +1,140 @@
 'use strict'
 
-var validator=require('validator');
-var path=require('path');
-var Resto=require('../models/resto');
+var validator = require('validator');
+var path = require('path');
+var Resto = require('../models/resto');
+var jwt = require('jsonwebtoken');
 
-var controller={
+var controller = {
 
-    getResto:(req,res)=>{
+    login: (req, res) => {
+        const user = { id: 3 };
+        const token = jwt.sign({ user }, 'clave secreta');
+
+        return res.status(200).send({
+            token,
+            message: 'Token bridado'
+        });
+
+    },
+
+    getResto: (req, res) => {
 
         //Tomamos id de url
-        var restoId=req.params.id;
+        var restoId = req.params.id;
 
         //Valido si el id existe
-        if(!restoId||restoId==null){
+        if (!restoId || restoId == null) {
             return res.status(404).send({
-                status:'error',
-                message:'No existe el resto'
+                status: 'error',
+                message: 'No existe el resto'
             });
         }
 
         //Busco el objeto en bd
-        Resto.findById(restoId,(err,resto)=>{
-            if(err||!resto){
+        Resto.findById(restoId, (err, resto) => {
+            if (err || !resto) {
                 return res.status(404).send({
-                    status:'error',
-                    message:'No existe el resto'
+                    status: 'error',
+                    message: 'No existe el resto'
                 });
             }
 
 
             //Lo retorno como JSON
             return res.status(200).send({
-                status:'succes',
+                status: 'succes',
                 resto
             });
         });
     },
 
-    getRestos:(req,res)=>{
-        var query=Resto.find({});
-        var last=req.params.last;
+    getRestos: (req, res) => {
+        jwt.verify(req.token, 'clave secreta', (err, data) => {
+            if (err) {
+                return res.status(403).send({
+                    status: 'error',
+                    message: 'Token no valido'
+                });
+            } else {
+                var query = Resto.find({});
+                var last = req.params.last;
 
-        if(last||last!=undefined){
-            query.limit(5);
-        }
+                if (last || last != undefined) {
+                    query.limit(5);
+                }
 
-        query.sort('-_id').exec((err,restos)=>{
-            if(err){
-                return res.status(500).send({
-                    status:'error',
-                    message:'Error al devolver restos'
+                query.sort('-_id').exec((err, restos) => {
+                    if (err) {
+                        return res.status(500).send({
+                            status: 'error',
+                            message: 'Error al devolver restos'
+                        });
+                    }
+                    if (!restos) {
+                        return res.status(404).send({
+                            status: 'error',
+                            message: 'No hay restos para mostrar'
+                        });
+                    }
+                    return res.status(200).send({
+                        status: 'success',
+                        restos,
+                        data
+                    });
                 });
             }
-            if(!restos){
-                return res.status(404).send({
-                    status:'error',
-                    message:'No hay restos para mostrar'
-                });
-            }
-            return res.status(200).send({
-                status:'success',
-                restos
-            });
-
-        });
-
-
+        })
     },
 
-    saveResto:(req,res)=>{
-        var params=req.body;
+    saveResto: (req, res) => {
+        var params = req.body;
 
         try {
             //Valido datos que llegan por ruta
-            var validate_name=!validator.isEmpty(params.name);
-            var validate_description=!validator.isEmpty(params.description);
+            var validate_name = !validator.isEmpty(params.name);
+            var validate_description = !validator.isEmpty(params.description);
             //var validate_menu=!validator.isEmpty(params.menu);
-            
+
         } catch (error) {
             return res.status(400).send({
-                status:'error',
-                message:'Faltan datos por enviar'
+                status: 'error',
+                message: 'Faltan datos por enviar'
             });
         }
 
-        if(validate_name && validate_description){
+        if (validate_name && validate_description) {
 
             //Creo el objeto a guardar
-            var resto=new Resto();
+            var resto = new Resto();
 
             //Se asignan valores a dato a guardar
-            resto.name=params.name;
-            resto.description=params.description;
-            resto.menu=params.menu;
-            resto.image=null;
+            resto.name = params.name;
+            resto.description = params.description;
+            resto.menu = params.menu;
+            resto.image = null;
 
 
             //Se guarda dato
-            resto.save((err,restoStored)=>{
-                if(err||!restoStored){
+            resto.save((err, restoStored) => {
+                if (err || !restoStored) {
                     return res.status(404).send({
-                        status:'error',
-                        message:'El resto no se guardo'
+                        status: 'error',
+                        message: 'El resto no se guardo'
                     });
                 }
                 return res.status(200).send({
-                    status:'success',
-                    resto:restoStored
+                    status: 'success',
+                    resto: restoStored
                 });
             });
-        }else{
+        } else {
             return res.status(410).send({
-                status:'error',
-                message:'Los datos no son validos'
+                status: 'error',
+                message: 'Los datos no son validos'
             });
         }
     }
 }
 
-module.exports=controller;
+module.exports = controller;
--- routes/restoRoutes.js
+++ routes/restoRoutes.js
@@ -6,8 +6,25 @@ var RestoController=require('../controllers/restoControllers');
 var router=express.Router();
 
 router.get('/resto/:id',RestoController.getResto);
-router.get('/resto',RestoController.getRestos);
+router.get('/resto',ensureToken,RestoController.getRestos);
 router.post('/resto',RestoController.saveResto);
+router.post('/login',RestoController.login);
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        return res.status(410).send({
+            status: 'error',
+            message: 'Token no existe'
+        });
+    }
+
+}
 
 
 module.exports=router;
